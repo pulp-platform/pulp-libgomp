c28fdbda249c4fae092ee2c18ef335c25d707514
Alessandro Capotondi <alessandro.capotondi@unibo.it>
2018-08-04 02:19:12 +0200

    moved .gnu.offload_funcs and .gnu.offload_vars to the L2

--- config/pulp/hero/offload-manager.h
+++ config/pulp/hero/offload-manager.h
@@ -38,7 +38,7 @@ extern void** offload_var_table;
 extern uint32_t nb_offload_vars;
 extern void *__OFFLOAD_TARGET_TABLE__[];
 
-// #define OFFLOAD_MANAGER_VERBOSE
+#define OFFLOAD_MANAGER_VERBOSE
 
 static inline void
 gomp_init_offload_manager ( )
@@ -50,6 +50,16 @@ gomp_init_offload_manager ( )
     offload_var_table  = ((void **) __OFFLOAD_TARGET_TABLE__)[2];
     nb_offload_vars  = ((uint32_t)((void **) __OFFLOAD_TARGET_TABLE__)[3] - (uint32_t) ((void **) __OFFLOAD_TARGET_TABLE__)[2]) / 0x4U;
 
+#ifdef OFFLOAD_MANAGER_VERBOSE
+    printf("nb_offload_funcs = %d\n", nb_offload_funcs);
+    printf("nb_offload_vars = %d\n", nb_offload_vars);
+printf("__OFFLOAD_TARGET_TABLE__[0] = %x\n", (uint32_t)__OFFLOAD_TARGET_TABLE__[0]);
+printf("__OFFLOAD_TARGET_TABLE__[1] = %x\n",(uint32_t)__OFFLOAD_TARGET_TABLE__[1]);
+printf("__OFFLOAD_TARGET_TABLE__[2] = %x\n",(uint32_t)__OFFLOAD_TARGET_TABLE__[2]);
+printf("__OFFLOAD_TARGET_TABLE__[3] = %x\n",(uint32_t)__OFFLOAD_TARGET_TABLE__[3]);
+
+#endif
+       
     mailbox_write(TO_RUNTIME | 2);
     mailbox_write(nb_offload_funcs);
     mailbox_write(nb_offload_vars);
@@ -57,11 +67,11 @@ gomp_init_offload_manager ( )
     if(nb_offload_funcs) {
         mailbox_write(TO_RUNTIME | nb_offload_funcs);
 #ifdef OFFLOAD_MANAGER_VERBOSE
-        printf("(offload_func_table = %x {", offload_func_table);
+        printf("(offload_func_table = %d {", offload_func_table);
 #endif
         for(i = 0; i < nb_offload_funcs; i++){
 #ifdef OFFLOAD_MANAGER_VERBOSE          
-            printf ("func%d: %x, ", i, offload_func_table[i]);
+            printf ("func%d: %d, ", i, offload_func_table[i]);
 #endif
             mailbox_write((uint32_t) offload_func_table[i]);
         }
